4cd7c3ea23f9c90563d796ab55adb75294a15625
Yanzhao Wu <yanzhaowumail@gmail.com>
2023-07-20 20:44:27 -0400

    add cifar example

---
+++ create_venv.sh
@@ -0,0 +1,2 @@
+python3 -m venv py3_mxnet
+
---
+++ install_mxnet.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install git unzip cmake build-essential
+sudo apt-get install liblapack* libopenblas* libblas*
+
+sudo apt-get install clang gfortran
